7d2edd13fb60a08e25b5ae48d3ff128643a85250
Rowe <accc45@outlook.com>
2022-11-13 18:19:31 +0900

    Initial Commit

---
+++ LocalNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotification
+//
+//  Created by 길지훈 on 2022/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
