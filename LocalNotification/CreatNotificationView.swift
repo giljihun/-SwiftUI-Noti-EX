cc773f2ba9429ca9f8b4d5b8bc07965b19071cc5
Rowe <accc45@outlook.com>
2022-11-15 15:33:30 +0900

    # 기본적인 리스트 뷰 생성 # authorization allow request 기능 # .granted or denied 여부에 따른 reload 기능 # 새 알림 버튼 생성

---
+++ LocalNotification/CreatNotificationView.swift
@@ -0,0 +1,8 @@
+//
+//  CreatNotificationView.swift
+//  LocalNotification
+//
+//  Created by 길지훈 on 2022/11/15.
+//
+
+import Foundation
---
+++ LocalNotification/NotificationListView.swift
@@ -0,0 +1,49 @@
+//
+//  NotificationListView.swift
+//  LocalNotification
+//
+//  Created by 길지훈 on 2022/11/14.
+//
+
+import SwiftUI
+
+struct NotificationListView: View {
+    @StateObject private var notificationManager = NotificationManager()
+    //@StateObject -> 뷰안에서 안전하게 ObservedObject 인스턴스를 만들 수 있다.
+    @State private var isCreatPresented = false
+    var body: some View {
+        List(notificationManager.notifications, id: \.identifier) { notification in
+            Text(notification.content.title)
+                .fontWeight(.semibold)
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle("Cool - Time")
+        .onAppear(perform: notificationManager.reloadAuthorizationStatus)
+        .onChange(of: notificationManager.authorizationStatus) { authorizationStatus in
+            switch authorizationStatus {
+            case .notDetermined: // When First Opening App.
+                //request authorization
+                notificationManager.requestAuthorization()
+            case .authorized:
+                //get local notifications
+                notificationManager.reloadLocalNotification()
+            default: //don't allow
+                break
+            }
+        }
+        .toolbar {
+            Button {
+                isCreatPresented = true
+            } label: {
+                Image(systemName: "plus.circle")
+                    .imageScale(.large)
+            }
+        }
+    }
+}
+
+struct NotificationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationListView()
+    }
+}
